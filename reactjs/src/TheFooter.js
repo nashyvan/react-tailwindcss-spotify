a6e2faa36c5a54fffd8210b2e6ca0979d0e50e5f
nashivan <yarik.nashivan@gmail.com>
2022-02-04 23:37:57 +0200

    reactjs an array of jsx elements

--- reactjs/src/TheFooter.js
+++ reactjs/src/TheFooter.js
@@ -2,12 +2,17 @@ function TheFooter() {
     return (
         <footer className="mt-auto mb-8 ml-6">
             <ul>
-                <li>
-                    <a className="text-[11px] hover:underline py-2" href="/">Cookies</a>
-                </li>
-                <li>
-                    <a className="text-[11px] hover:underline py-2" href="/">Privacy</a>
-                </li>
+                {
+                    ['Cookies', 'Privacy'].map((label) =>
+                        (
+                            <li key={label}>
+                                <a className="text-[11px] hover:underline py-2" href="/">
+                                    {label}
+                                </a>
+                            </li>
+                        )
+                    )
+                }
             </ul>
         </footer>
     )
--- reactjs/src/TheNav.js
+++ reactjs/src/TheNav.js
@@ -1,29 +1,49 @@
 import { HomeIcon, SearchIcon, ViewBoardsIcon, PlusCircleIcon, HeartIcon } from '@heroicons/react/outline'
 
+const activeNavItemClasses = 'flex items-center text-white bg-[#282828] mx-2 px-4 py-2 rounded'
+const navItemClasses = 'flex items-center hover:text-white mx-2 px-4 py-2 rounded duration-300'
+
+const navItems = [
+    {
+        label: 'Home',
+        classes: activeNavItemClasses,
+        icon: <HomeIcon className="h-6 w-6" />,
+    },
+    {
+        label: 'Search',
+        classes: navItemClasses,
+        icon: <SearchIcon className="h-6 w-6" />,
+    },
+    {
+        label: 'Your library',
+        classes: `${navItemClasses} mb-6`,
+        icon: <ViewBoardsIcon className="h-6 w-6" />,
+    },
+    {
+        label: 'Create playlist',
+        classes: navItemClasses,
+        icon: <PlusCircleIcon className="h-6 w-6" />,
+    },
+    {
+        label: 'Liked songs',
+        classes: navItemClasses,
+        icon: <HeartIcon className="h-6 w-6" />,
+    },
+]
+
 function TheNav() {
     return (
         <nav>
-            <a className="flex items-center text-white bg-[#282828] mx-2 px-4 py-2 rounded" href="/">
-                <HomeIcon className="h-6 w-6" />
-                <span className="ml-4 text-sm font-semibold">Home</span>
-            </a>
-            <a className="flex items-center hover:text-white mx-2 px-4 py-2 rounded duration-300" href="/">
-                <SearchIcon className="h-6 w-6" />
-                <span className="ml-4 text-sm font-semibold">Search</span>
-            </a>
-            <a className="flex items-center hover:text-white mx-2 px-4 py-2 rounded duration-300 mb-6"
-               href="/">
-                <ViewBoardsIcon className="h-6 w-6" />
-                <span className="ml-4 text-sm font-semibold">Your library</span>
-            </a>
-            <a className="flex items-center hover:text-white mx-2 px-4 py-2 rounded duration-300" href="/">
-                <PlusCircleIcon className="h-6 w-6" />
-                <span className="ml-4 text-sm font-semibold">Create playlist</span>
-            </a>
-            <a className="flex items-center hover:text-white mx-2 px-4 py-2 rounded duration-300" href="/">
-                <HeartIcon className="h-6 w-6" />
-                <span className="ml-4 text-sm font-semibold">Liked songs</span>
-            </a>
+            {
+                navItems.map(({ classes, icon, label }) =>
+                    (
+                        <a className={ classes } href="/" key={ label }>
+                            { icon }
+                            <span className="ml-4 text-sm font-semibold">{ label }</span>
+                        </a>
+                    )
+                )
+            }
         </nav>
     )
 }
