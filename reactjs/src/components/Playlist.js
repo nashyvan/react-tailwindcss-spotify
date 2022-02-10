0dcc6a3bccac0ec0596d1641bae290f988f13ac7
nashivan <yarik.nashivan@gmail.com>
2022-02-10 14:00:13 +0200

    reactjs context menu refactoring

--- reactjs/src/components/Playlist.js
+++ reactjs/src/components/Playlist.js
@@ -4,6 +4,29 @@ import PlaylistTitle from './PlaylistTitle'
 import PlaylistDescription from './PlaylistDescription'
 import PlaylistContextMenu from './PlaylistContextMenu'
 
+const menuItems = [
+    {
+        label: 'Add to your library'
+    },
+    {
+        label: 'Share',
+        subMenuItems: [
+            {
+                label: 'Copy link to playlist'
+            },
+            {
+                label: 'Embed playlist'
+            }
+        ]
+    },
+    {
+        label: 'About recommendations'
+    },
+    {
+        label: 'Open in Desktop app'
+    },
+]
+
 function Playlist() {
     return (
         <a className="relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group"
@@ -14,7 +37,7 @@ function Playlist() {
             </div>
             <PlaylistTitle />
             <PlaylistDescription />
-            <PlaylistContextMenu />
+            <PlaylistContextMenu menuItems={ menuItems } classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default divide-y divide-[#3e3e3e] whitespace-nowrap z-10 hidden group-hover:block" />
         </a>
     )
 }
--- reactjs/src/components/PlaylistContextMenu.js
+++ reactjs/src/components/PlaylistContextMenu.js
@@ -1,31 +1,8 @@
 import PlaylistContextMenuItem from "./PlaylistContextMenuItem";
 
-const menuItems = [
-    {
-        label: 'Add to your library'
-    },
-    {
-        label: 'Share',
-        subMenuItems: [
-            {
-                label: 'Copy link to playlist'
-            },
-            {
-                label: 'Embed playlist'
-            }
-        ]
-    },
-    {
-        label: 'About recommendations'
-    },
-    {
-        label: 'Open in Desktop app'
-    },
-]
-
-function PlaylistContextMenu() {
+function PlaylistContextMenu({ classes, menuItems }) {
     return (
-        <ul className="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default divide-y divide-[#3e3e3e] whitespace-nowrap z-10 hidden group-hover:block">
+        <ul className={ classes }>
             {
                 menuItems.map(({ label, subMenuItems }) => (
                     <PlaylistContextMenuItem key="{ label }" subMenuItems={ subMenuItems }>{ label }</PlaylistContextMenuItem>
